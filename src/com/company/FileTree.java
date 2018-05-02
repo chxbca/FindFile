bb2a060ead5e3280c076a654503d688d6d4bc2e9
chxbca <chxbca@gmail.com>
2018-05-02 22:06:54 +0800

    My first github progrom

---
+++ src/com/company/FileTree.java
@@ -0,0 +1,101 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+class FileTree {
+    private final Node<File> root;
+    private final PrintWriter out = new PrintWriter("out\\production\\FindFile\\com\\company\\Log.txt");
+    private final Map<String, HashSet<Node<File>>> hashMap = new HashMap<>(4096);
+
+    FileTree(File file) throws IOException {
+        root = new Node<>(file);
+        add2HashMap(root);
+        addFile(root);
+        out.close();
+    }
+
+    private void addFile(final Node<File> node) {
+        try {
+            File[] files = node.item.listFiles();
+            if (files == null)
+                return;
+            int num = files.length;
+            @SuppressWarnings("unchecked")
+            Node<File>[] nodes = new Node[num];
+            for (int i = 0; i < num; i++) {
+                nodes[i] = new Node<>(files[i]);
+//                nodes[i].prev = node;
+                add2HashMap(nodes[i]);
+                if (files[i].isDirectory()) {
+                    addFile(nodes[i]);
+                }
+            }
+            node.nextTree = nodes;
+        } catch (NullPointerException e) {
+            System.err.println(node);
+            out.println(node);
+        }
+    }
+
+    void show() {
+        System.out.println(root.item);
+        showNext(root);
+    }//前序遍历
+
+    private void showNext(Node<File> node) {
+        try {
+            Node<File>[] next = node.nextTree;
+            for (Node<File> fileNode : next) {
+                System.out.println(fileNode.item);
+                if (fileNode.item.isDirectory()) {
+                    showNext(fileNode);
+                }
+            }
+        } catch (NullPointerException e) {
+            System.err.println("无法访问该目录 " + node.item);
+        }
+    }
+
+    boolean search(String fileName) {
+        HashSet<Node<File>> hashSet = hashMap.get(fileName);
+        if (hashSet == null) {
+            return false;
+        }
+        for (Node node : hashSet) {
+            System.out.println(node);
+        }
+        return true;
+    }
+
+    private void add2HashMap(Node<File> node) {
+        String value = node.item.getName();
+        if (hashMap.get(value) == null) {
+            HashSet<Node<File>> hashSet = new HashSet<>();
+            hashSet.add(node);
+            hashMap.put(value, hashSet);
+        } else {
+            hashMap.get(value).add(node);
+        }
+    }
+
+    private static class Node<E> {
+        private E item;
+        //        private Node<E> prev;
+        private Node<E>[] nextTree;
+
+        private Node(E data) {
+            this.item = data;
+        }
+
+        @Override
+        public String toString() {
+            return item.toString();
+        }
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Program Files\\Java";
+        String fileName = "java.exe";
+        File file = new File(path);
+        FileTree fileTree = new FileTree(file);
+
+        fileTree.show();
+
+        System.out.println("搜索路径  ：" + path);
+        System.out.println("搜索文件名：" + fileName);
+
+        if (!fileTree.search(fileName)) {
+            System.out.println("File Not Found");
+        }
+    }
+}
